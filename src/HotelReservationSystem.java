abb1dcaf95855340ca54109809c48f83f2b4f238
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-09-30 18:05:38 +0530

    HotelReservationSystem.java

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 public class HotelReservationSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
     private static final String username = "root";
-    private static final String password = "#Rahul7505";
+    private static final String password = "******";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try {
